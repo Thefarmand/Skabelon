4be22a0b3132cb21cc56cc07f8e957fb907e768d
Thefarmand <31881760+Thefarmand@users.noreply.github.com>
2018-12-18 21:43:44 +0100

    Add project files.

---
+++ ModelLibrary/Model/Cars.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLibrary.Model
+{
+    public class Cars
+    {
+        public int car_id { get; set; }
+        public string car_model { get; set; }
+        public string car_type { get; set; }
+        public int car_year { get; set; }
+        public int car_price { get; set; }
+
+        //public Cars(string model, string type, int year, int price)
+        //{
+        //    _model = model;
+        //    _type = type;
+        //    _year = year;
+        //    _price = price;
+        //}
+
+        //public Cars()
+        //{
+        //}
+
+        //public string Model
+        //{
+        //    get => _model;
+        //    set => _model = value;
+        //}
+
+        //public string Type
+        //{
+        //    get => _type;
+        //    set => _type = value;
+        //}
+
+        //public int Year
+        //{
+        //    get => _year;
+        //    set => _year = value;
+        //}
+
+        //public int Price
+        //{
+        //    get => _price;
+        //    set => _price = value;
+        //}
+
+        public override string ToString()
+        {
+            return ($"{nameof(car_id)}:{car_id}, {nameof(car_model)}:{car_model}, {nameof(car_type)}:{car_type}, {nameof(car_year)}:{car_year}, {nameof(car_price)}:{car_price}");
+        }
+    }
+}
---
+++ Skabelon/Controllers/SkabelonController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelLibrary;
+using ModelLibrary.Model;
+
+namespace Skabelon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkabelonController : ControllerBase
+    {
+
+        private string connectionstring =
+                "Server=tcp:thefarmand.database.windows.net,1433;Initial Catalog=Thefarmand;Persist Security Info=False;User ID=Thefarmand;Password=Killer1963;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+            ;
+
+        // GET: api/Skabelon
+        [HttpGet(Name = "GetAll")]
+        public IEnumerable<Cars> Get()
+        {
+            SqlConnection conn = new SqlConnection(connectionstring);
+            SqlCommand query = new SqlCommand("SELECT * FROM cars", conn);
+            conn.Open();
+            SqlDataReader laeser = query.ExecuteReader();
+            List<Cars> carList = new List<Cars>();
+            if (laeser.HasRows)
+            {
+                while (laeser.Read())
+                {
+                    Cars cl = new Cars
+                    {
+                        car_id = Convert.ToInt32(laeser[0]),
+                        car_model = Convert.ToString(laeser[1]),
+                        car_type = Convert.ToString(laeser[2]),
+                        car_year = Convert.ToInt32(laeser[3]),
+                        car_price = Convert.ToInt32(laeser[4])
+                    };
+                    carList.Add(cl);
+                }
+            }
+            conn.Close();
+            return carList;
+        }
+
+        // GET: api/cars/1
+        [HttpGet("{id}", Name = "GetOne")]
+        public List<Cars> GetOne(int id)
+        {
+            SqlConnection conn = new SqlConnection(connectionstring);
+            SqlCommand query = new SqlCommand($"SELECT * FROM  cars WHERE car_id={id}", conn);
+            conn.Open();
+            SqlDataReader laeser = query.ExecuteReader();
+            List<Cars> us = new List<Cars>();
+
+            if (laeser.HasRows)
+            {
+                while (laeser.Read())
+                {
+                    Cars cs = new Cars
+                    {
+                        car_id = Convert.ToInt32(laeser[0]),
+                        car_model = Convert.ToString(laeser[1]),
+                        car_type = Convert.ToString(laeser[2]),
+                        car_year = Convert.ToInt32(laeser[3]),
+                        car_price = Convert.ToInt32(laeser[4])
+                    };
+                    us.Add(cs);
+                }
+            }
+            conn.Close();
+            return us;
+        }
+
+        // POST: /api/skabelon/model/Ford/type/Mustang/year/1966/price/85000 indsætter ny car
+        [HttpPost("model/{model}/type/{type}/year/{year}/price/{price}")]
+        public void Put(string model, string type, int year, int price)
+        {
+            SqlConnection conn = new SqlConnection(connectionstring);
+            SqlCommand query =
+                new SqlCommand(
+                    $"INSERT INTO cars(car_model, car_type, car_year, car_price) VALUES('{model}', '{type}', {year}, {price})",
+                    conn);
+            conn.Open();
+            query.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        // PUT: api/skabelon/id/5/model/Ford/type/Mustang/year/1966/price/85000 opdaterer id 1
+        [HttpPut("id/{id}/model/{model}/type/{type}/year/{year}/price/{price}")]
+        public void Put(int id, string model, string type, int year, int price)
+        {
+            SqlConnection conn = new SqlConnection(connectionstring);
+            SqlCommand query =
+                new SqlCommand(
+                    $"UPDATE cars SET car_model = '{model}', car_type = '{type}', car_year = {year}, car_price = {price} WHERE car_id = {id}",
+                    conn);
+            conn.Open();
+            query.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        // DELETE: api/skabelon/6 sletter car nr. 16
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            SqlConnection conn = new SqlConnection(connectionstring);
+            SqlCommand query = new SqlCommand($"DELETE FROM cars WHERE car_id = {id}", conn);
+            conn.Open();
+            query.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
